e55d5e6542c9784e0693cdddf3eeb43765b80c39
NanaWhan <cherishronaldo07@gmail.com>
2023-08-26 21:25:33 +0000

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Offers from "./Pages/Offers";
 function App() {
   return (
-
 <>
-<h1>Hello Nana</h1>
+<Router>
+  <Routes>
+<Route path="/Home" element={<Home/>}/>
+<Route path="/Profile" element={<Profile/>}/>
+<Route path="/sign-up" element={<SignUp/>}/>
+<Route path="/sign-in" element={<SignIn/>}/>
+<Route path="/forgot-password" element={<ForgotPassword/>}/>
+<Route path="/Offers" element={<Offers/>}/>
+  </Routes>
+</Router>
 </>
   );
 }
